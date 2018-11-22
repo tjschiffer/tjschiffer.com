c93c04c8764ad381d0f934c97c2857867cd31e25
Unknown <tjschiffer@gmail.com>
2018-11-21 23:32:47 -0500

    Improve theme coloring

--- build/main.js
+++ build/main.js
@@ -33,25 +33,39 @@ const store = new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production'
 });
 
-const colors = [
-  '#400000',
-  '#000040',
-  '#002000',
-  '#351000',
-  '#001020',
-  '#250025'
-];
-
-const getApprovedColor = () => {
-  const x = Math.sin(Math.round(new Date().getTime() / 10000)) / 2 + 0.5;
-  const index = Math.floor(x * (colors.length - 1));
-  return colors[index];
+/**
+ * Deterministic function that creates a
+ * non-grey dark color using UTC as a seed.
+ *
+ * See https://www.wolframalpha.com/input/?i=sin(x)%2F1.5+%2B+1%2F3,+sin(x+%2B+PI*+2%2F3)%2F1.5+%2B+1%2F3,+sin(x+%2B+PI+*+4%2F3)%2F1.5+%2B+1%2F3
+ * to understand how the shifted sin function is used to
+ * create deep reds, greens, blues, and in-betweens.
+ * (When the y value goes below 0, that value will be 0).
+ *
+ * @returns {string}
+ */
+const getCalculatedHexColor = () => {
+  const seed = Math.round(new Date().getTime() / 100) / 100000;
+  // Use shifted sin functions to create values between 0 and 50
+  return [
+    n => Math.sin(n),
+    n => Math.sin(n + Math.PI * 2/3),
+    n => Math.sin(n + Math.PI * 4 / 3)
+  ].reduce((hexColor, func) => {
+    // Use provided function to generate a value between 0 and 50
+    const colorVal = Math.round(Math.max(func(seed) / 1.5 + 1 / 3, 0) * 50);
+    // Add the number as a hex number making sure it's a padding two digit number
+    hexColor = hexColor + ('0' + colorVal.toString(16)).slice(-2);
+    return hexColor;
+  }, '#');
 };
 
+// Vuex goes not have lifecycle events so just take the
+// store and wrap it to check the color theme every 5 seconds
 setInterval(() => {
-  store.commit('changeColor', getApprovedColor());
-}, 1000);
-store.commit('changeColor', getApprovedColor());
+  store.commit('changeColor', getCalculatedHexColor());
+}, 5000);
+store.commit('changeColor', getCalculatedHexColor());
 
 
 window.onload = () => {
